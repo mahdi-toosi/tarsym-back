f45694375f78e2062e855ac2313c29d387974d4f
mahdi toosi <pugnacious0571@gmail.com>
2020-08-02 10:36:30 +0430

    socket is removed , we should use rest for host ,,,

--- webpack.mix.js
+++
@@ -1,9 +0,0 @@
-const mix = require('laravel-mix');
-
-mix
-    .stylus('resources/styles/main.styl', 'public/css')
-    .copy('resources/images/', 'public/images')
-    .copy('resources/styles/libs/fonts', 'public/fonts')
-    // .js('resources/javascripts/main.js', 'public/js')
-    // .copy('resources/styles/libs/leaflet.css', 'public/css/')
-    .disableNotifications();
