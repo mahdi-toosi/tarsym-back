c7a8416fa0f58dc9eb8f43ce5b8fca66a631d312
mahdi toosi <pugnacious0571@gmail.com>
2020-08-05 12:08:47 +0430

    project uploaded successfully , go for trello list

---
+++ public/dist/precache-manifest.b311cd255f851298486e2bb89d8f9df2.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61341df1eac93cb7c81e",
+    "url": "/css/app.efcb810c.css"
+  },
+  {
+    "revision": "953b927cb1be9a6490ef",
+    "url": "/css/chunk-286d98a8.53316d16.css"
+  },
+  {
+    "revision": "e0602982fc56bb37fac9",
+    "url": "/css/chunk-a62d3944.e86b86cc.css"
+  },
+  {
+    "revision": "efe4c23ca9be4cdd2a39",
+    "url": "/css/chunk-c31999e8.94dd2540.css"
+  },
+  {
+    "revision": "cb091fe7d12355b04431",
+    "url": "/css/chunk-vendors.c8a8cc96.css"
+  },
+  {
+    "revision": "b6c45d92f5517961190cd955b4c1e4b7",
+    "url": "/fonts/eot/IRANSansWeb(FaNum)_Black.eot"
+  },
+  {
+    "revision": "4abe296380edacb9f146cd778a94f43d",
+    "url": "/fonts/eot/IRANSansWeb(FaNum)_Bold.eot"
+  },
+  {
+    "revision": "ad395cc9a045cb4059bed55605a611e6",
+    "url": "/fonts/eot/IRANSansWeb(FaNum)_Light.eot"
+  },
+  {
+    "revision": "30ecec094f809f90a3e4c121115cc8ca",
+    "url": "/fonts/eot/IRANSansWeb(FaNum)_Medium.eot"
+  },
+  {
+    "revision": "bbb57c7053ed930508b718bc70802932",
+    "url": "/fonts/eot/IRANSansWeb(FaNum)_UltraLight.eot"
+  },
+  {
+    "revision": "94c311fd48c9362dea340aa3a29e3567",
+    "url": "/fonts/eot/IRANSansWeb.eot"
+  },
+  {
+    "revision": "20f2dc0a09e36bed1b999d5236ec4014",
+    "url": "/fonts/ttf/IRANSansWeb(FaNum)_Black.ttf"
+  },
+  {
+    "revision": "ff320f78af3a0fd44f2ee2993559fa9f",
+    "url": "/fonts/ttf/IRANSansWeb(FaNum)_Bold.ttf"
+  },
+  {
+    "revision": "bd26f02a2febca2229ccf2c4d37ee3f7",
+    "url": "/fonts/ttf/IRANSansWeb(FaNum)_Light.ttf"
+  },
+  {
+    "revision": "8789622647008ae1b00f6a890b49916e",
+    "url": "/fonts/ttf/IRANSansWeb(FaNum)_Medium.ttf"
+  },
+  {
+    "revision": "cb2ad2322140b8c86f1acb43710f9ad1",
+    "url": "/fonts/ttf/IRANSansWeb(FaNum)_UltraLight.ttf"
+  },
+  {
+    "revision": "e9908f05e5771638e40913309b784a17",
+    "url": "/fonts/ttf/IRANSansWeb.ttf"
+  },
+  {
+    "revision": "bea60ea788538ed6b635ad0c519fb0b8",
+    "url": "/fonts/woff/IRANSansWeb(FaNum)_Black.woff"
+  },
+  {
+    "revision": "753b3827c415580e864a545d1a860a5a",
+    "url": "/fonts/woff/IRANSansWeb(FaNum)_Bold.woff"
+  },
+  {
+    "revision": "25bd95ce239f04e9457b5cf1c7dac593",
+    "url": "/fonts/woff/IRANSansWeb(FaNum)_Light.woff"
+  },
+  {
+    "revision": "fe1913144aa13ac4b31777a96230fed1",
+    "url": "/fonts/woff/IRANSansWeb(FaNum)_Medium.woff"
+  },
+  {
+    "revision": "658b85640048a3b6d9893cbb16a44cfb",
+    "url": "/fonts/woff/IRANSansWeb(FaNum)_UltraLight.woff"
+  },
+  {
+    "revision": "bd6f69a8db87710b2f3fcd6ef75bd3e2",
+    "url": "/fonts/woff/IRANSansWeb.woff"
+  },
+  {
+    "revision": "24f304ff5075d348a40cdbcddff6a3d3",
+    "url": "/fonts/woff2/IRANSansWeb(FaNum)_Black.woff2"
+  },
+  {
+    "revision": "ceaf6d89af9fb96d0466b26d6f1c022a",
+    "url": "/fonts/woff2/IRANSansWeb(FaNum)_Bold.woff2"
+  },
+  {
+    "revision": "3f707510b31097b91baee0aaa50a57ce",
+    "url": "/fonts/woff2/IRANSansWeb(FaNum)_Light.woff2"
+  },
+  {
+    "revision": "62528a12d06f0745f8a43c0fd1318862",
+    "url": "/fonts/woff2/IRANSansWeb(FaNum)_Medium.woff2"
+  },
+  {
+    "revision": "6e57754a4baf5c1bcf358a9270049b3b",
+    "url": "/fonts/woff2/IRANSansWeb(FaNum)_UltraLight.woff2"
+  },
+  {
+    "revision": "eb5adaac0d814e1e8e5cbd75efb9db3e",
+    "url": "/fonts/woff2/IRANSansWeb.woff2"
+  },
+  {
+    "revision": "cd8ef1d8f47a946a6a6ea23aa1140ae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61341df1eac93cb7c81e",
+    "url": "/js/app.f7bc2e39.js"
+  },
+  {
+    "revision": "953b927cb1be9a6490ef",
+    "url": "/js/chunk-286d98a8.cd975e81.js"
+  },
+  {
+    "revision": "a44925cddb89508e58ab",
+    "url": "/js/chunk-2d0c498f.f0481426.js"
+  },
+  {
+    "revision": "150466e35571d035d823",
+    "url": "/js/chunk-344b43d5.c9aa0814.js"
+  },
+  {
+    "revision": "e0602982fc56bb37fac9",
+    "url": "/js/chunk-a62d3944.c8b467ec.js"
+  },
+  {
+    "revision": "efe4c23ca9be4cdd2a39",
+    "url": "/js/chunk-c31999e8.d25516ea.js"
+  },
+  {
+    "revision": "cb091fe7d12355b04431",
+    "url": "/js/chunk-vendors.db4e9d8c.js"
+  },
+  {
+    "revision": "f192a4b4498f75aed0014d4d08a9733a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
